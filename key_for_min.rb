d9f25f805e2e28720abcde4506a5adcd67de1964
Michael McCullough <michael.mccullough21@gmail.com>
2019-06-24 19:58:45 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,12 +3,12 @@
 
 def key_for_min_value(name_hash)
   lowest = 1000
-  winner = name_hash
+  winner = ""
    name_hash.each do |name|
      if value < lowest
        lowest = value
        winner = "#{name}"
      end
   end
-  lowest
+  winner
 end
