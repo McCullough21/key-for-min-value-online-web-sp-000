2ff882f0df45341ab71e8630b48135b0cfc26648
Michael McCullough <michael.mccullough21@gmail.com>
2019-06-24 19:58:29 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,10 +2,12 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  lowest = name_hash[0]
+  lowest = 1000
+  winner = name_hash
    name_hash.each do |name|
-     if name_hash["#{name}"] < lowest
-       lowest = name_hash["#{name}"]
+     if value < lowest
+       lowest = value
+       winner = "#{name}"
      end
   end
   lowest
